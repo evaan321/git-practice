876ccec95b6e5ce783d15027d12d9d185c08852d
shuk29 <shuklaghosh775.com>
2025-04-13 00:54:30 +0600

    Shuk2

--- lib/main.dart
+++ lib/main.dart
@@ -59,9 +59,8 @@ class _MyHomePageState extends State<MyHomePage> {
   void _incrementCounter() {
     setState(() {
 
-      _counter++;
-      _counter+=1;
-      _counter++;
+      _counter--;
+      
     });
   }
 
